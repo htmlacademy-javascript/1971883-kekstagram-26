78f0a79eb19d0c7dd8844765d3337b42201a3221
Vladislav <typlomi@gmail.com>
2022-06-05 19:50:43 +0300

    Фиксит ошибки линта

--- js/main.js
+++ js/main.js
@@ -2,24 +2,28 @@
 
 const getRandomNumber = function (minNumber, maxNumber) {
   if (minNumber < 0 || maxNumber < 0) {
-    return console.log('Ошибка. диапазон чисел может быть только положительный, включая 0')
-  };
+    return 'Ошибка. диапазон чисел может быть только положительный, включая 0';
+  }
 
   if (minNumber >= maxNumber) {
-    return Math.floor(Math.random() * (minNumber - maxNumber + 1)) + maxNumber  // При обратном условии вычетает из минимального максимальное
-  };
+    return Math.floor(Math.random() * (minNumber - maxNumber + 1)) + maxNumber;  // При обратном условии вычетает из минимального максимальное
+  }
 
   return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber; // Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 };
+
+getRandomNumber(1, 10);
 
 // Функция для проверки максимальной длины строки
 
 const checkStringLength = function (checkingString, maxStringLength) {
   if (checkingString.length > maxStringLength) {
     return false;
-  };
+  }
 
   return true;
 };
+
+checkStringLength('dskafgdnsaggkn', 25);
 
 
